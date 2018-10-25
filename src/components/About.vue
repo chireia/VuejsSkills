c0a592b1ee8c347de3f9ead8b53f83d2fb4c297f
Rodrigo Chireia <rodrigochireia@hotmail.com>
2018-10-24 22:22:06 -0400

    First commit!

---
+++ src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="about">
+        <h1>Olá, Essa é minha página "Sobre"</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ut. Perspiciatis voluptatem libero debitis, fugiat quo labore, asperiores eveniet sequi dicta assumenda officiis? Atque debitis ut alias dignissimos soluta rerum!</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+};
+</script>
